6fa60d55fd5d347c563950bdc87be35856ea281c
Tracy Worthington <worthington_t@hotmail.com>
2018-08-10 07:39:08 -0700

    1.0

--- MyManagementFunctions/SourceControlQueue/run.ps1
+++ MyManagementFunctions/SourceControlQueue/run.ps1
@@ -1,46 +1,25 @@
  
 Import-Module AzureRM.Profile
 
-    $vaultName = "vault4bxudj6xo65by"
-    $secretKey ="Gittoken"
+$in = Get-Content $triggerInput | ConvertFrom-Json
 
-    if ($req_query_TenantID) 
-    {
-        $TenantID = $req_query_TenantID
-    }
-
-    if ($req_query_Sub) 
-    {
-        $SubID = $req_query_Sub
-    }
-
-    if ($req_query_resourcegroup) 
-    {
-        $AppServiceResourceGroupName = $req_query_resourcegroup
-    }
-    
-    if ($req_query_AppServiceName) 
-    {
-        $AppServiceName = $req_query_AppServiceName
-    }
-
-    if ($req_query_Repo) 
-    {
-        $Repo = $req_query_Repo
-    }
-    
-    if ($req_query_Branch) 
-    {
-        $Branch = $req_query_Branch
-    }
+Write-Output "PowerShell script processed queue message '$in'"
 
     $Props = @{
-        RepoUrl = "$repo"
-        Branch = "$Branch"
+        RepoUrl = $($in.Repo)
+        Branch = "$($in.Branch)"
         isManualIntegration = "false" 
     }
 
+    $tenantId = $($in.TenantId)
+    $subid = $($in.subid)
+    $AppServiceResourceGroupName = $($in.ResourceGroup)
+    $AppServiceName = $($in.AppServiceName)
+
+    $vaultName = "vault4bxudj6xo65by"
+    $secretKey ="Gittoken"
     $apiversion = "2017-09-01"
+
     $resourceURi = "https://vault.azure.net"
     $tokenAuthUri = "$env:MSI_EndPoint"+ "?resource=$resourceUri&api-version=$apiversion"
     $tokenResponse = invoke-RestMethod -method Get -Headers @{"Secret"="$env:MSI_Secret"} -uri $tokenAuthURI
@@ -59,16 +38,12 @@ Import-Module AzureRM.Profile
     # Use the access token to sign in under the MSI service principal
     Login-AzureRmAccount -AccessToken $accesstoken  -Tenant $tenantId -AccountId "$env:MSI_Secret" -SubscriptionID $subid
     
-if($token) {
-
+    if($token) {
     $PropertiesObject = @{
         token = "$token";
     }
     Set-AzureRmResource -PropertyObject $PropertiesObject -ResourceId /providers/Microsoft.Web/sourcecontrols/GitHub -ApiVersion 2015-08-01 -Force
 
     Set-AzureRmResource -PropertyObject $Props -ResourceGroupName $AppServiceResourceGroupName -ResourceType Microsoft.Web/sites/sourcecontrols -ResourceName $AppServiceName/web -ApiVersion 2015-08-01 -Force
-
-    Out-File -Encoding Ascii -FilePath $res -inputObject "Hello $AppServiceName"
-} else {
-    Out-File -Encoding Ascii -FilePath $res -inputObject "No Git Token found"
-}
+    
+    }
